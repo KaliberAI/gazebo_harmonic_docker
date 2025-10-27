c81a90c3a9b7c1cfcf1b2b65f94b8e3220c9b9fc
liver121888 <liver121888@gmail.com>
2025-10-27 15:50:06 -0700

    Modify compose.yaml to match readme.md, update compose build for image _name

--- compose_build.bash
+++ compose_build.bash
@@ -5,7 +5,7 @@ DISTRO=jazzy
 # Change these values to use different versions of ROS or different base images. The rest of the script should be left unchanged.
 BASE_IMAGE=osrf/ros
 BASE_TAG=$DISTRO-desktop-full
-IMAGE_NAME=docker_control_simulation
+IMAGE_NAME=gazebo_harmonic
 IMAGE_TAG=0.1
 
 USERNAME=ros
